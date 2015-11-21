0108875b2444ba29bfebdbbf5562460dbf4a1ca1
ChrisBellew <cjbellew@gmail.com>
2015-11-21 16:09:22 +0800

    Made proxy which serialises three numbers into JSON

--- JobSpawn.Common/Proxy.cs
+++ JobSpawn.Common/Proxy.cs
@@ -6,6 +6,7 @@ using System.Reflection.Emit;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JobSpawn.Common
 {
@@ -28,7 +29,7 @@ namespace JobSpawn.Common
         public TContract As<TContract>()
         {
             var type = typeof(TContract);
-
+            
             AssemblyName assemblyName = new AssemblyName();
             assemblyName.Name = "JobSpawnProxyAssembly";
             AssemblyBuilder assemblyBuilder = Thread.GetDomain().DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
@@ -38,19 +39,65 @@ namespace JobSpawn.Common
 
             foreach (var methodInfo in type.GetMethods())
             {
-                var method = typeBuilder.DefineMethod(methodInfo.Name, MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Final, methodInfo.ReturnType, methodInfo.GetParameters().Select(x => x.ParameterType).ToArray());
+                var parameters = methodInfo.GetParameters().Select(x => x.ParameterType).ToArray();
+                var method = typeBuilder.DefineMethod(methodInfo.Name, MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Final, methodInfo.ReturnType, parameters);
 
                 var ilGenerator = method.GetILGenerator();
-                ilGenerator.DeclareLocal(typeof(int));
 
-                ilGenerator.Emit(OpCodes.Nop);
+                ilGenerator.DeclareLocal(typeof(object[]));
+                ilGenerator.DeclareLocal(typeof(string));
+
                 ilGenerator.Emit(OpCodes.Ldc_I4_3);
+                ilGenerator.Emit(OpCodes.Newarr, typeof(object));
+                ilGenerator.Emit(OpCodes.Dup);
+                ilGenerator.Emit(OpCodes.Ldc_I4_0);
+                ilGenerator.Emit(OpCodes.Ldarg_1);
+                ilGenerator.Emit(OpCodes.Box, typeof(int));
+                ilGenerator.Emit(OpCodes.Stelem_Ref);
+                ilGenerator.Emit(OpCodes.Dup);
+                ilGenerator.Emit(OpCodes.Ldc_I4_1);
+                ilGenerator.Emit(OpCodes.Ldarg_2);
+                ilGenerator.Emit(OpCodes.Box, typeof(int));
+                ilGenerator.Emit(OpCodes.Stelem_Ref);
+                ilGenerator.Emit(OpCodes.Dup);
+                ilGenerator.Emit(OpCodes.Ldc_I4_2);
+                ilGenerator.Emit(OpCodes.Ldarg_3);
+                ilGenerator.Emit(OpCodes.Box, typeof(int));
+                ilGenerator.Emit(OpCodes.Stelem_Ref);
+                ilGenerator.Emit(OpCodes.Stloc_0);
+                ilGenerator.Emit(OpCodes.Ldloc_0);
+                ilGenerator.Emit(OpCodes.Call, typeof(JsonConvert).GetMethod("SerializeObject", new [] { typeof(object) }));
+                ilGenerator.Emit(OpCodes.Stloc_1);
+                Label targetInstruction = ilGenerator.DefineLabel();
+                ilGenerator.Emit(OpCodes.Br_S, targetInstruction);
+                ilGenerator.MarkLabel(targetInstruction);
+                ilGenerator.Emit(OpCodes.Ldloc_1);
+                ilGenerator.Emit(OpCodes.Ret);
+
+                /*ilGenerator.DeclareLocal(typeof(int));
+                ilGenerator.Emit(OpCodes.Ldarg_1);
                 ilGenerator.Emit(OpCodes.Stloc_0);
                 Label targetInstruction = ilGenerator.DefineLabel();
                 ilGenerator.Emit(OpCodes.Br_S, targetInstruction);
                 ilGenerator.MarkLabel(targetInstruction);
                 ilGenerator.Emit(OpCodes.Ldloc_0);
-                ilGenerator.Emit(OpCodes.Ret);
+                ilGenerator.Emit(OpCodes.Ret);*/
+
+
+
+                /*ilGenerator.DeclareLocal(typeof(int));
+                ilGenerator.Emit(OpCodes.Ldarg_1);
+                ilGenerator.Emit(OpCodes.Ldarg_2);
+                ilGenerator.Emit(OpCodes.Add);
+                ilGenerator.Emit(OpCodes.Ret);*/
+
+                /*ilGenerator.DeclareLocal(typeof(int));
+                ilGenerator.Emit(OpCodes.Ldarg_1);
+                ilGenerator.Emit(OpCodes.Ret);*/
+
+                /*ilGenerator.DeclareLocal(typeof(int));
+                ilGenerator.Emit(OpCodes.Ldc_I4_3);
+                ilGenerator.Emit(OpCodes.Ret);*/
 
                 typeBuilder.DefineMethodOverride(method, methodInfo);
             }
---
+++ JobSpawn.ILTests/ILTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using JobSpawn.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace JobSpawn.ILTests
+{
+    [TestClass]
+    public class ILTests
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            ITestOne testOne = new SpawnProxy().As<ITestOne>();
+            Assert.AreEqual(3, testOne.GetNumber());
+        }
+
+        public class TestOneClass : ITestOne
+        {
+            public int GetNumber()
+            {
+                return 3;
+            }
+        }
+
+        public interface ITestOne
+        {
+            int GetNumber();
+        }
+
+        [TestMethod]
+        public void TestTwo()
+        {
+            ITestTwo testTwo = new SpawnProxy().As<ITestTwo>();
+            Assert.AreEqual(4, testTwo.GetNumber(4));
+        }
+
+        public class TestTwoClass : ITestTwo
+        {
+            public int GetNumber(int number)
+            {
+                return number;
+            }
+        }
+
+        public interface ITestTwo
+        {
+            int GetNumber(int number);
+        }
+
+        [TestMethod]
+        public void TestThree()
+        {
+            ITestThree testThree = new SpawnProxy().As<ITestThree>();
+            Assert.AreEqual(9, testThree.GetNumber(4, 5));
+        }
+
+        public class TestThreeClass : ITestThree
+        {
+            public int GetNumber(int number, int numberTwo)
+            {
+                return number + numberTwo;
+            }
+        }
+
+        public interface ITestThree
+        {
+            int GetNumber(int number, int numberTwo);
+        }
+
+        [TestMethod]
+        public void TestFour()
+        {
+            ITestFour testFour = new SpawnProxy().As<ITestFour>();
+            Assert.AreEqual(1, testFour.ReturnFirstInt(1, 2, 3));
+        }
+
+        public class TestFourClass : ITestFour
+        {
+            public int ReturnFirstInt(int numberOne, int numberTwo, int numberThree)
+            {
+                return 0;
+            }
+        }
+
+        public interface ITestFour
+        {
+            int ReturnFirstInt(int numberOne, int numberTwo, int numberThree);
+        }
+
+        [TestMethod]
+        public void TestFive()
+        {
+            ITestFive testFive = new SpawnProxy().As<ITestFive>();
+            Assert.AreEqual("[1,2,3]", testFive.SerialiseParameters(1, 2, 3));
+        }
+
+        public class TestFiveClass : ITestFive
+        {
+            public string SerialiseParameters(int numberOne, int numberTwo, int numberThree)
+            {
+                object[] array = { numberOne, numberTwo, numberThree };
+                return JsonConvert.SerializeObject(array);
+            }
+        }
+
+        public interface ITestFive
+        {
+            string SerialiseParameters(int numberOne, int numberTwo, int numberThree);
+        }
+    }
+}
